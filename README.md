9dfbaecaa00bbbdb93b2160e631a1e179f8de1b1
Amohelang Mohlomi <mohlomiamohelang3@gmail.com>
2025-09-27 19:29:17 +0200

    Added html template

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Personal_BrainBloom
 A Personal BrainBloom is an AI-powered web app designed to help users learn more effectively and confidently.
+
+# To run 
+pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from google import genai
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv() 
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key' 
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 
 
