f80d8e6e5fea0e47985c08fff7441005805ad670
Amohelang Mohlomi <mohlomiamohelang3@gmail.com>
2025-09-27 20:06:44 +0200

    Linked the home page to the ai page

--- app.py
+++ app.py
@@ -13,5 +13,18 @@ app.secret_key = 'your_secret_key'
 def home():
     return render_template("index.html")
 
+@app.route("/bloomie-ai")
+def talk_to_bloomie():
+    return render_template("bloomie-ai.html")
+
+def build_prompt():
+    context="""You are personalized ai study assistant called Bloomie, you are meant to provide:  
+    Simplified topic explanations, Curated learning resources ,Quick quizzes – Fun and interactive self-testing,
+    Think of yourself as a friend who explains tough concepts and ensures people understand."""
+    
+    return context
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
